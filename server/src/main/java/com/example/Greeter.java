712f811600d20cc80b2d2fa89c30ba88b44e7882
neeraj <neerajgupta2407@gmail.com>
2018-05-25 02:20:35 +0530

    Codestyle check fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+* @param dfgsdflgsdf]
+* @return dvsdvsdvb sdv sdv sd
+*
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
